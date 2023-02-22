6a2e4704b930692770bbb89ce1649bf217ecf85b
chenweirui <chenweirui@chenweiruideMacBook-Pro-3.local>
2023-02-22 21:36:05 +0800

    update

--- single-bankend/readliine.js
+++ single-bankend/readliine.js
@@ -29,34 +29,48 @@ let defaultObj = {
     wxcode: '',
     image_list: '',
     desc: '',
-    openid: '',
-    created_at: '',
-    updated_at: ''
+    openid: ''
 }
 
 const rl = readline.createInterface({
     input: fs.createReadStream('./database_export-AnEVXYZYQfOf.json')
 });
+// 定义map去重
+const map = {};
+// 用户总量
+const userList = [];
 rl.on('line', (line) => {
     let timeLine = JSON.parse(line);
+    let newObj = {};
     // 获取所有keys
+    if(map[timeLine.openid]) {
+        return;
+    }
+    map[timeLine.openid] = 1;
     let objKeys = Object.keys(defaultObj);
     for (const key of objKeys) {
         if(key == 'avatar_url') {
-            timeLine['avatar_url'] = defaultObj['avatarUrl'];
+            newObj['avatar_url'] = timeLine['avatarUrl'];
         } else if (key == 'nick_name') {
-            timeLine['avatar_url'] = defaultObj['nickName'];
+            newObj['nick_name'] = timeLine['nickName'];
         } else {
-            timeLine[key] ? null : timeLine[key] = defaultObj[key];
+            timeLine[key] ? newObj[key] = timeLine[key] :  newObj[key] = defaultObj[key];
         }
     }
+    console.log('newObj:' + newObj.class);
+    console.log('timeLine:' + timeLine.class)
+
+    userList.push(newObj);
     // timeLine['publishTime'] = Date.now() + '';
     // timeLine['updateTime'] = Date.now() + '';
     // console.log('line:' + line._id);
-    console.log(JSON.stringify(timeLine));
+    // console.log(JSON.stringify(newObj));
     // console.log(line)
 });
-        
+
 rl.on('close', ()=> {
-    console.log('closed')
+    // console.log(JSON.stringify(userList));
+    console.log(userList.length);   
+    fs.writeFileSync('userList.js', JSON.stringify(userList));
+    console.log('closed');
 });
